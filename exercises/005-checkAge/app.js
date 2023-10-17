a8478c005b091139293c58bd240cb9600088d582
McMartinezc <psyence_mc@hotmail.com>
2023-10-17 12:18:04 +0200

    20 finalizados

--- exercises/005-checkAge/app.js
+++ exercises/005-checkAge/app.js
@@ -1,4 +1,7 @@
 function checkAge(name, age) {
   // your code here
-  
+  return (age >= 21) ? `Welcome, ${name}!` : `Go home, ${name}!`;
 }
+
+console.log(checkAge("Mc", 21));
+console.log(checkAge("Mc", 20));
